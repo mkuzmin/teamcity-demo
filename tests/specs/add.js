1b9fb337e5350eebefded93dacebb0a3dd001987
Michael Kuzmin <mkuzmin@gmail.com>
2017-10-09 19:57:50 +0300

    tests

---
+++ tests/specs/add.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+
+describe('Checklist', function() {
+    it('should submit login form', function () {
+        browser.url('/');
+        $('form').waitForExist(5000);
+
+        var count = browser.elements('span.description').value.length;
+        var value = require("uuid").v4();
+        browser.setValue('#newItem', value);
+        browser.submitForm('form');
+
+        browser.waitUntil(function () {
+            var l = browser.elements('span.description').value.length;
+            return l == count + 1;
+        }, 5000);
+
+        var actualValue = browser.elementIdText(browser.elements('span.description').value[count].ELEMENT).value;
+        assert.equal(actualValue, value)
+    });
+});
